aeece6d442ce48b83fab74734695eb0c2e427b51
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-02 14:25:09 -0500

    rubocop auto

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,25 +5,23 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
+      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality > 0
           if item.name != 'Sulfuras, Hand of Ragnaros'
             item.quality = item.quality - 1
           end
         end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+      elsif item.quality < 50
+        item.quality = item.quality + 1
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+          end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
@@ -42,10 +40,8 @@ class GildedRose
           else
             item.quality = item.quality - item.quality
           end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+        elsif item.quality < 50
+          item.quality = item.quality + 1
         end
       end
     end

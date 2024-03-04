1a5dbc099b2f6a9021bfb85d18acb3f1ddab7de0
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-04 13:05:28 -0500

    update_quality fewer ifs

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,44 +5,25 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      elsif item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
+
+      if item.name == '+5 Dexterity Vest' || item.name == 'Elixir of the Mongoose'
+        item.sell_in -= 1
+        item.quality -= 1 if item.quality > 0
+        item.quality -= 1 if item.sell_in < 0 && item.quality > 0
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
+      if item.name == 'Aged Brie'
+        item.sell_in -= 1
+        item.quality += 1 if item.quality < 50
+        item.quality += 1 if item.sell_in < 0 && item.quality < 50
       end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        elsif item.quality < 50
-          item.quality = item.quality + 1
-        end
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        item.sell_in -= 1
+        item.quality += 1
+        item.quality += 1 if item.sell_in < 10
+        item.quality += 1 if item.sell_in < 5
+        item.quality = 50 if item.quality > 50
+        item.quality = 0 if item.sell_in < 0
       end
     end
   end

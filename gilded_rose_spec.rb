f31f0919254a668e30c1319c12e509ab95bf8314
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-04 11:25:45 -0500

    spec cleanup

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -15,33 +15,17 @@ describe GildedRose do
 
       it 'sets sell_in' do
         days.each do |day|
-          puts "-------- day #{day} --------" if debug
-          item = Item.new(
-            name,
-            sell_in_by_day[day],
-            quality_by_day[day] || 0
-          )
-          puts item.inspect if debug
-          items = [item]
-          described_class.new(items).update_quality
-          result = items[0]
-          expect(result.sell_in).to eq(sell_in_by_day[day + 1])
+          item = Item.new(name, sell_in_by_day[day], quality_by_day[day] || 0)
+          described_class.new([item]).update_quality
+          expect(item.sell_in).to eq(sell_in_by_day[day + 1])
         end
       end
 
       it 'sets quality' do
         days.each do |day|
-          puts "-------- day #{day} --------" if debug
-          item = Item.new(
-            name,
-            sell_in_by_day[day],
-            quality_by_day[day] || 0
-          )
-          puts item.inspect if debug
-          items = [item]
-          described_class.new(items).update_quality
-          result = items[0]
-          expect(result.quality).to eq quality_by_day[day + 1]
+          item = Item.new(name, sell_in_by_day[day], quality_by_day[day] || 0)
+          described_class.new([item]).update_quality
+          expect(item.quality).to eq quality_by_day[day + 1]
         end
       end
     end
@@ -74,16 +58,14 @@ describe GildedRose do
       }
     end
 
-    context 'with Sulfuras, Hand of Ragnaros A' do
+    context 'with Sulfuras, Hand of Ragnaros' do
       it_behaves_like 'known pattern', {
         name: 'Sulfuras, Hand of Ragnaros',
         sell_in_by_day: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         quality_by_day: [80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80,
                          80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80]
       }
-    end
 
-    context 'with Sulfuras, Hand of Ragnaros B' do
       it_behaves_like 'known pattern', {
         name: 'Sulfuras, Hand of Ragnaros',
         sell_in_by_day: [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -92,25 +74,21 @@ describe GildedRose do
       }
     end
 
-    context 'with Backstage passes to a TAFKAL80ETC concert A' do
+    context 'with Backstage passes to a TAFKAL80ETC concert' do
       it_behaves_like 'known pattern', {
         name: 'Backstage passes to a TAFKAL80ETC concert',
         sell_in_by_day: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15],
         quality_by_day: [20, 21, 22, 23, 24, 25, 27, 29, 31, 33, 35, 38, 41, 44, 47, 50, 0, 0, 0,
                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       }
-    end
 
-    context 'with Backstage passes to a TAFKAL80ETC concert B' do
       it_behaves_like 'known pattern', {
         name: 'Backstage passes to a TAFKAL80ETC concert',
         sell_in_by_day: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20],
         quality_by_day: [49, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 0, 0,
                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       }
-    end
 
-    context 'with Backstage passes to a TAFKAL80ETC concert C' do
       it_behaves_like 'known pattern', {
         name: 'Backstage passes to a TAFKAL80ETC concert',
         sell_in_by_day: [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20, -21, -22, -23, -24, -25],
@@ -119,8 +97,8 @@ describe GildedRose do
       }
     end
 
+    # TODO: not implemented
     xcontext 'Conjured Mana Cake' do
-      # TODO: not implemented
       it_behaves_like 'known pattern', {
         name: 'Conjured Mana Cake',
         sell_in_by_day: [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20, -21, -22, -23, -24, -25],

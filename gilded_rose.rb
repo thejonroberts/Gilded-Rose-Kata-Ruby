bd1cbdcc5bbfb5f79c7b01ab5f872483c093fd09
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-04 14:51:11 -0500

    use some update methods

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,27 +5,42 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      next if item.name == 'Sulfuras, Hand of Ragnaros'
+      return no_update item if item.name == 'Sulfuras, Hand of Ragnaros'
 
-      if item.name == '+5 Dexterity Vest' || item.name == 'Elixir of the Mongoose'
-        item.sell_in -= 1
-        item.quality -= 1 if item.quality > 0
-        item.quality -= 1 if item.sell_in < 0 && item.quality > 0
-      end
-      if item.name == 'Aged Brie'
-        item.sell_in -= 1
-        item.quality += 1 if item.quality < 50
-        item.quality += 1 if item.sell_in < 0 && item.quality < 50
-      end
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        item.sell_in -= 1
-        item.quality += 1
-        item.quality += 1 if item.sell_in < 10
-        item.quality += 1 if item.sell_in < 5
-        item.quality = 50 if item.quality > 50
-        item.quality = 0 if item.sell_in < 0
-      end
+      return brie_update item if item.name == 'Aged Brie'
+
+      return backstage_pass_update item if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+
+      normal_update item
     end
+  end
+
+  def no_update(item)
+    item
+  end
+
+  def normal_update(item)
+    item.sell_in -= 1
+    item.quality -= 1 if item.quality > 0
+    item.quality -= 1 if item.sell_in < 0 && item.quality > 0
+    item
+  end
+
+  def brie_update(item)
+    item.sell_in -= 1
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.sell_in < 0 && item.quality < 50
+    item
+  end
+
+  def backstage_pass_update(item)
+    item.sell_in -= 1
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 10
+    item.quality += 1 if item.sell_in < 5
+    item.quality = 50 if item.quality > 50
+    item.quality = 0 if item.sell_in < 0
+    item
   end
 end
 
@@ -37,6 +52,10 @@ class Item
     @sell_in = sell_in
     @quality = quality
   end
+
+  def limit_quality; end
+  def update_quality; end
+  def update_sell_in; end
 
   def to_s
     "#{@name}, #{@sell_in}, #{@quality}"

32f2630b711ebe2328e3c568552d8fee0fcee9d0
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-04 19:56:59 -0500

    implement conjured mana cake

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -97,8 +97,7 @@ describe GildedRose do
       }
     end
 
-    # TODO: not implemented
-    xcontext 'Conjured Mana Cake' do
+    context 'with Conjured Mana Cake' do
       it_behaves_like 'known pattern', {
         name: 'Conjured Mana Cake',
         sell_in_by_day: [5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19, -20, -21, -22, -23, -24, -25],

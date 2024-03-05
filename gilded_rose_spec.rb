4ac4b3ea537ee5cc0c289f498efe23f0dbc9a15c
Jon Roberts <jon.roberts.8@gmail.com>
2024-03-04 23:21:39 -0500

    add requirements to specs

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -27,6 +27,33 @@ describe GildedRose do
     end
 
     context 'with normal items' do
+      # All items have a SellIn value which denotes the number of days we have to sell the items
+      # All items have a Quality value which denotes how valuable the item is
+      # At the end of each day our system lowers both values for every item
+      # Pretty simple, right? Well this is where it gets interesting:
+
+      # Once the sell by date has passed, Quality degrades twice as fast
+      # The Quality of an item is never negative
+      # The Quality of an item is never more than 50
+
+      it 'reduces quality by 1 per day' do
+        item = Item.new('foo', 10, 10)
+        results = described_class.new([item]).update_quality
+        expect(results[0].quality).to eq 9
+      end
+
+      it 'reduces quality by 2 per day after sell date' do
+        item = Item.new('foo', 0, 10)
+        results = described_class.new([item]).update_quality
+        expect(results[0].quality).to eq 8
+      end
+
+      it 'does not reduce quality below zero' do
+        item = Item.new('foo', 0, 0)
+        results = described_class.new([item]).update_quality
+        expect(results[0].quality).to eq 0
+      end
+
       it_behaves_like 'known pattern',
                       '+5 Dexterity Vest',
                       [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14,
@@ -48,6 +75,26 @@ describe GildedRose do
     end
 
     context 'with aged items' do
+      # "Aged Brie" actually increases in Quality the older it gets
+
+      it 'increases quality by 1 per day' do
+        item = Item.new('Aged Brie', 10, 10)
+        results = described_class.new([item]).update_quality
+        expect(results[0].quality).to eq 11
+      end
+
+      it 'increases quality by 2 per day after sell date' do
+        item = Item.new('Aged Brie', 0, 10)
+        results = described_class.new([item]).update_quality
+        expect(results[0].quality).to eq 12
+      end
+
+      it 'does not increase quality above fifty' do
+        item = Item.new('Aged Brie', 0, 50)
+        results = described_class.new([item]).update_quality
+        expect(results[0].quality).to eq 50
+      end
+
       it_behaves_like 'known pattern',
                       'Aged Brie',
                       [2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16, -17, -18, -19,
@@ -57,6 +104,7 @@ describe GildedRose do
     end
 
     context 'with collectable Item' do
+      # "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
       it_behaves_like 'known pattern',
                       'Sulfuras, Hand of Ragnaros',
                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -72,6 +120,10 @@ describe GildedRose do
     end
 
     context 'with backstage passes' do
+      # "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+      # Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+      # Quality drops to 0 after the concert
+
       it_behaves_like 'known pattern',
                       'Backstage passes to a TAFKAL80ETC concert',
                       [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10,
@@ -92,6 +144,12 @@ describe GildedRose do
                        -18, -19, -20, -21, -22, -23, -24, -25],
                       [49, 50, 50, 50, 50, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                        0]
+    end
+
+    context 'with conjured items' do
+      # We have recently signed a supplier of conjured items. This requires an update to our system:
+      # "Conjured" items degrade in Quality twice as fast as normal items
+      # Once the sell by date has passed, Quality degrades twice as fast
     end
   end
 end
